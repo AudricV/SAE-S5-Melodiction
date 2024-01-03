c485a720e6d6980d3c71eb2f3d272bafc3eaa1a4
Mathieu YAHIA-AMAR <92095174+Matithieuu@users.noreply.github.com>
2024-01-03 22:50:18 +0100

    fix: route of the app

--- melodiction/src/App.tsx
+++ melodiction/src/App.tsx
@@ -1,17 +1,19 @@
-import './App.css';
-import { MelodyProvider } from './context/melodyContetxt';
 // @ts-ignore
-import MainPage from './pages/Main/main';
 import { Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Layout from './pages/Layout';
+import MelodyPage from './pages/Chat';
 
 
 function App() {
   return (
-    <MelodyProvider>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </MelodyProvider>
+    <Routes>
+      <Route element={<Layout />} >
+        <Route path="/" element={<Landing />} />
+        <Route path="/melody/:id" element={<MelodyPage />} />
+      </Route>
+    </Routes>
+
   );
 }
 
