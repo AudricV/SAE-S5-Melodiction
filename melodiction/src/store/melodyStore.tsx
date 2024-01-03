64d6b4a04c8d4322d646577f020ec0c27556fee6
Mathieu YAHIA-AMAR <92095174+Matithieuu@users.noreply.github.com>
2024-01-03 22:46:21 +0100

    feat: use store from Zustand instead of context

---
+++ melodiction/src/store/melodyStore.tsx
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+import Melody from '../data/melody';
+
+type MelodyState = {
+  selectedMelody: Melody | null;
+  setSelectedMelody: (Melody: Melody | null) => void;
+}; 
+
+// Create a store using Zustand
+export const useMelodyStore = create<MelodyState>((set) => ({
+  selectedMelody: null,
+  setSelectedMelody: (Melody) => set(() => ({ selectedMelody: Melody })),
+}));
+
+// Usage in components remains the same
+// const { selectedMelody, setSelectedMelody, ... } = useMelodyStore();
