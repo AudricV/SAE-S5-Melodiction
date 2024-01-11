37adfec1fc7b62528b0d3412feafd5ee8ebd5206
AudricV <74829229+AudricV@users.noreply.github.com>
2024-01-11 11:28:13 +0100

    refactor(SynthType): Move SynthType enum in an external file

--- melodiction/src/tools/sound_playback_manager.ts
+++ melodiction/src/tools/sound_playback_manager.ts
@@ -1,14 +1,7 @@
 import * as Tone from 'tone';
 import { Instrument, InstrumentOptions } from 'tone/build/esm/instrument/Instrument';
 import { convertLetterToNote } from './char_converter';
-
-/**
- * Types of synths.
- */
-export enum SynthType {
-    SYNTH
-    // TODO: add other synth types when we will use them
-}
+import { SynthType } from "./synth_types";
 
 /**
  * A class managing all synths used and playback of notes.
