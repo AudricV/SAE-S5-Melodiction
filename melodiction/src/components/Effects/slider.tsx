c9b7f31b8104e20cde7cf6f5fd85a0753b7b209c
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-01-18 11:47:09 +0100

    feat: Add VerticalSlider component

---
+++ melodiction/src/components/Effects/slider.tsx
@@ -0,0 +1,47 @@
+import { Box, Slider, Typography } from "@mui/material";
+
+type VerticalSliderProps = {
+    name: string;
+    value: number | undefined;
+    defaultValue: number;
+    max: number;
+    step?: number;
+    onChange: (event: Event, newValue: number | number[]) => void;
+    label: string;
+    style?: React.CSSProperties;
+};
+
+/**
+ *  A vertical slider component.
+ * @param param0 Takes a name, value, defaultValue, max, step, onChange, label and style as props.
+ *
+ */
+export const VerticalSlider: React.FC<VerticalSliderProps> = ({ name, value, defaultValue, max, step, onChange, label, style }) => {
+    return (
+        <Box sx={{ height: '100%', flexDirection: 'column', display: 'flex', alignItems: 'center' }}>
+            <Slider
+                name={name}
+                value={value ?? 0}
+                max={max}
+                step={step}
+                onChange={onChange}
+                sx={{
+                    '& input[type="range"]': {
+                        WebkitAppearance: 'slider-vertical',
+                    },
+                }}
+                orientation="vertical"
+                defaultValue={defaultValue}
+                aria-label={label}
+                valueLabelDisplay="auto"
+                onKeyDown={(event: React.KeyboardEvent) => {
+                    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+                        event.preventDefault();
+                    }
+                }}
+                style={style}
+            />
+            <Typography id="vertical-slider">{label}</Typography>
+        </Box>
+    );
+};
