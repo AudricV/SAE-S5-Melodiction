f9de41f89c246cc867167c4729ed4ee75019735a
AudricV <74829229+AudricV@users.noreply.github.com>
2024-01-18 10:09:41 +0100

    test: Update Melody tests to apply effect changes

--- melodiction/src/tests/data/melody.test.ts
+++ melodiction/src/tests/data/melody.test.ts
@@ -1,30 +1,58 @@
 import { expect, test } from 'vitest';
 import Melody, { InvalidTimestampError } from "../../data/melody";
+import {SynthType} from "../../tools/synth_types.ts";
+import {Effect, ReverbEffect} from "../../tools/effect.ts";
 
 test("A melody instance with correct values should not throw an error",
-    () => expect(() => new Melody("azerty", "Melody", "tada", 0)).not
-        .toThrowError(Error));
+    () => expect(() => new Melody(
+        "azerty",
+        "Melody",
+        "tada",
+        0,
+        new Map().set(SynthType.SYNTH, [{
+            decay: 12
+        } as ReverbEffect])))
+        .not.toThrowError(Error));
 
 test("Invalid last modified timestamp in constructor should throw an InvalidTimestampError",
     () => expect(() =>
-        new Melody("azerty", "Melody", "tada", -1)).toThrowError(InvalidTimestampError));
+        new Melody(
+            "azerty",
+            "Melody",
+            "tada",
+            -1,
+            new Map().set(SynthType.SYNTH, [{
+                decay: 12
+            } as ReverbEffect])))
+        .toThrowError(InvalidTimestampError));
 
 test("Getters should return data provided", () => {
     const melodyId = "azerty";
     const melodyName = "Melody";
     const melodyText = "tada";
     const melodyLastModifiedTimestamp = new Date().getTime();
+    const effects: Map<SynthType, Effect[]> = new Map().set(SynthType.SYNTH, [{
+        decay: 12
+    } as ReverbEffect]);
 
-    const melody = new Melody(melodyId, melodyName, melodyText, melodyLastModifiedTimestamp);
+    const melody = new Melody(melodyId, melodyName, melodyText, melodyLastModifiedTimestamp, effects);
 
     expect(melody.getId()).toEqual(melodyId);
     expect(melody.getName()).toEqual(melodyName);
     expect(melody.getMelodyText()).toEqual(melodyText);
     expect(melody.getLastModifiedTimestamp()).toEqual(melodyLastModifiedTimestamp);
+    expect(melody.getEffects()).toEqual(effects);
 });
 
 test("Setters should update data", () => {
-    const melody = new Melody("azerty", "Melody", "tada", 0);
+    const melody = new Melody(
+        "azerty",
+        "Melody",
+        "tada",
+        0,
+        new Map().set(SynthType.SYNTH, [{
+            decay: 12
+        } as ReverbEffect]));
 
     const newMelodyName = "My new melody name";
     melody.setName(newMelodyName);
@@ -41,7 +69,15 @@ test("Setters should update data", () => {
 });
 
 test("Set an invalid timestamp show throw an Error",
-    () => expect(() => new Melody("azerty", "Melody", "tada", 0).setLastModifiedTimestamp(-1))
+    () => expect(() => new Melody(
+        "azerty",
+        "Melody",
+        "tada",
+        0,
+        new Map().set(SynthType.SYNTH, [{
+            decay: 12
+        } as ReverbEffect]))
+        .setLastModifiedTimestamp(-1))
         .toThrow(InvalidTimestampError));
 
 test("Create an InvalidTimestampError should not throw an error",
