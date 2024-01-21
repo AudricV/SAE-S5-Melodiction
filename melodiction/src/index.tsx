01378de240865d023fb7942342da8b6ba8511215
AudricV <74829229+AudricV@users.noreply.github.com>
2024-01-21 23:18:32 +0100

    build: Add repo name in paths

--- melodiction/src/index.tsx
+++ melodiction/src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/SAE-S5-Melodiction">
       <App />
     </Router>
   </React.StrictMode>
