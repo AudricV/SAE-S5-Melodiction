7cfc5b83ddccf8feb838bef0ad7af0bcc70419b6
AudricV <74829229+AudricV@users.noreply.github.com>
2024-01-24 16:28:58 +0100

    build: Remove base property in vite config

--- melodiction/vite.config.ts
+++ melodiction/vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: "/SAE-S5-Melodiction",
     plugins: [react()],
     test: {
         globals: true,
