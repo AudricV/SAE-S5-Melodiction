f4b31578421f21d19cecd6ba8087680b955a0ec8
AudricV <74829229+AudricV@users.noreply.github.com>
2024-01-25 16:09:21 +0100

    fix(useManageMusic): Fix duplicate events and new melody text

--- melodiction/src/hooks/manage_music.tsx
+++ melodiction/src/hooks/manage_music.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import { useMelodyStore } from "../store/melodyStore";
 import MelodiesStorage from "../data/storage/melodies_storage";
 import { useAudioStore } from "../store/effectsStore";
+import Melody from "../data/melody";
 
-export const tempo = 0.16 ;
+export const tempo: number = 0.16;
 
 export const useManageMusic = (melodiesStorage: MelodiesStorage) => {
     const { selectedMelody, setSelectedMelody } = useMelodyStore();
 
     const [isEditing, setIsEditing] = useState(false);
-    const [isPlaying, setIsPlaying] = useState(false);
 
     const { synthType, soundPlaybackManager } = useAudioStore();
 
@@ -36,33 +36,31 @@ export const useManageMusic = (melodiesStorage: MelodiesStorage) => {
     };
 
     const handleTextChange = (text: string) => {
-        text = selectedMelody?.getMelodyText() + text;
-        const newMelody = selectedMelody;
-        newMelody?.setMelodyText(text);
-        setSelectedMelody(newMelody);
-        soundPlaybackManager.playText(text, tempo);
+        if (selectedMelody == null) {
+            return;
+        }
+
+        setSelectedMelody(new Melody(
+            selectedMelody.getId(),
+            selectedMelody.getName(),
+            selectedMelody.getMelodyText() + text,
+            selectedMelody.getLastModifiedTimestamp(),
+            selectedMelody.getEffects()));
     };
 
     const handlePlayMusic = () => {
-        if (selectedMelody && !isPlaying) {
-            setIsPlaying(true);
+        if (selectedMelody) {
             soundPlaybackManager.setSelectedSynth(synthType);
             // TODO: try to not apply effects each time the play button is pressed
             //  This change requires several refactors
             selectedMelody.getEffects().get(synthType)?.forEach(effect =>
                 soundPlaybackManager.addOrReplaceSynthEffect(synthType, effect));
             soundPlaybackManager.playText(selectedMelody.getMelodyText(), tempo);
-            setTimeout(() => {
-                setIsPlaying(false);
-            }, selectedMelody.getMelodyText().length * (tempo *  1.2) * 1000);
         }
     }
 
     const handleStopMusic = () => {
-        if (isPlaying) {
-            setIsPlaying(false);
-            soundPlaybackManager.stopPlayback();
-        }
+        soundPlaybackManager.stopPlayback();
     }
 
     const selectedMelodyIndex = melodiesStorage.getMelodiesList()?.findIndex(m => m.getId() === selectedMelody?.getId());
